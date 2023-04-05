e6c46c138aef662a98979904f30358956706e1b3
PipadaGaurav <2019pcecsgaurav58@poornima.org>
2023-04-05 13:57:51 +0530

    updated toggle counter button

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,30 @@
 import { createStore } from "redux";
+const initialState={counter:0,showCounter:true}
 
-const counterReducder = (state = {counter:0},actions) => {
+const counterReducder = (state = initialState,actions) => {
     if(actions.type === 'increment'){
         return{
-            counter:state.counter+1
+            counter:state.counter+1,
+            showCounter:state.showCounter
         }
     }
     if(actions.type === 'decrement'){
         return{
-            counter:state.counter-1
+            counter:state.counter-1,
+            showCounter:state.showCounter
+        }
+    }
+    if(actions.type === 'increase'){
+        return{
+            counter:state.counter+actions.amount,
+            showCounter:state.showCounter
+        }
+    }
+
+    if(actions.type==='toggle'){
+        return{
+            counter:state.counter,
+            showCounter:!state.showCounter
         }
     }
 
