8b52bbc0dbbf15c09bf384496639160cf7a3dcd2
PipadaGaurav <2019pcecsgaurav58@poornima.org>
2023-04-05 14:32:58 +0530

    Connecting to redux toolkit and migrating into it

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,6 +1,7 @@
 import { useSelector,useDispatch} from 'react-redux';
 
 import classes from './Counter.module.css';
+import { counterActions } from '../store/index';
 
 const Counter = () => {
   const dispatch = useDispatch();
@@ -8,30 +9,35 @@ const Counter = () => {
   const show =useSelector(state => state.showCounter)
 
   const incrementHandler = () =>{
-    dispatch({type:'increment'});
+    // dispatch({type:'increment'});
+    dispatch(counterActions.increment());
+   
   }
 
   const increment = () =>{
-    dispatch({type:'increase',amount:5});
+    // dispatch({type:'increase',amount:5});
+    dispatch(counterActions.increase(5));
   }
 
   const decrementHandler = () =>{
-    dispatch({type:'decrement'});
+    // dispatch({type:'decrement'});
+    dispatch(counterActions.decrement());
   }
 
   const toggleCounterHandler = () => {
-    dispatch({type:'toggle'})
+    // dispatch({type:'toggle'})
+    dispatch(counterActions.toggleCounter());
   };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       {show && <div className={classes.value}>{counter}</div>}
-      <div>
+      {show && <div>
           <button onClick={incrementHandler}>Increment</button>
           <button onClick={decrementHandler}>Decrement</button>
           <button onClick={increment}>Increment by 5</button>
-      </div>
+      </div>}
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -1,36 +1,61 @@
-import { createStore } from "redux";
+import { createSlice, configureStore } from '@reduxjs/toolkit';
+
 const initialState={counter:0,showCounter:true}
 
-const counterReducder = (state = initialState,actions) => {
-    if(actions.type === 'increment'){
-        return{
-            counter:state.counter+1,
-            showCounter:state.showCounter
-        }
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+      increment(state) {
+        state.counter++;
+      },
+      decrement(state) {
+        state.counter--;
+      },
+      increase(state, action) {
+        state.counter = state.counter + action.payload;
+      },
+      toggleCounter(state) {
+        state.showCounter = !state.showCounter;
+      }
     }
-    if(actions.type === 'decrement'){
-        return{
-            counter:state.counter-1,
-            showCounter:state.showCounter
-        }
-    }
-    if(actions.type === 'increase'){
-        return{
-            counter:state.counter+actions.amount,
-            showCounter:state.showCounter
-        }
-    }
+  });
 
-    if(actions.type==='toggle'){
-        return{
-            counter:state.counter,
-            showCounter:!state.showCounter
-        }
-    }
+// const counterReducder = (state = initialState,actions) => {
+//     if(actions.type === 'increment'){
+//         return{
+//             counter:state.counter+1,
+//             showCounter:state.showCounter
+//         }
+//     }
+//     if(actions.type === 'decrement'){
+//         return{
+//             counter:state.counter-1,
+//             showCounter:state.showCounter
+//         }
+//     }
+//     if(actions.type === 'increase'){
+//         return{
+//             counter:state.counter+actions.amount,
+//             showCounter:state.showCounter
+//         }
+//     }
 
-    return state;
-};
+//     if(actions.type==='toggle'){
+//         return{
+//             counter:state.counter,
+//             showCounter:!state.showCounter
+//         }
+//     }
 
-const store = createStore(counterReducder);
+//     return state;
+// };
 
+// const store = createStore(counterReducder);
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+  });
+
+export const counterActions = counterSlice.actions;
 export default store;
